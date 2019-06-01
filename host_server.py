292d0a2215106f73169b51cdf27297470385773c
Manas Mahapatra <zed009@Manass-MacBook-Pro.local>
2019-06-01 12:22:18 +0530

    Adding a Dummy host reporting server for testing purposes.

---
+++ host_server.py
@@ -0,0 +1,24 @@
+import socket
+from threading import *
+#Dummy Reporting Server for testing
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "172.17.9.65"
+port = 6001
+serversocket.bind((host, port))
+
+class client(Thread):
+    def __init__(self, socket, address):
+        Thread.__init__(self)
+        self.sock = socket
+        self.addr = address
+        self.start()
+
+    def run(self):
+        while 1:
+            print(self.sock.recv(1024).decode())
+
+serversocket.listen(5)
+print ('reporting server started and listening')
+while 1:
+    clientsocket, address = serversocket.accept()
+    client(clientsocket, address)
